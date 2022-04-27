f13a68a7c7a1b23a5b378255ad8f93ddecd904a9
clbrunet <brunet.clement5@gmail.com>
2022-04-27 09:49:15 +0200

    fix: fix internet checksum calculation by adding the carrys

--- src/ft_ping/checksum.c
+++ src/ft_ping/checksum.c
@@ -1,31 +1,39 @@
 #include <assert.h>
+#include <netinet/in.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdint.h>
 
 #include "ft_ping/checksum.h"
 
-uint16_t get_checksum(void *addr, size_t size)
+static uint16_t get_data_sum(uint16_t *addr, size_t size)
 {
 	assert(addr != NULL);
 
 	uint32_t sum = 0;
 	size_t remaining_bytes_count = size;
-	uint16_t *iter = addr;
 
 	while (remaining_bytes_count >= 2) {
-		sum += *iter;
-		if (sum > 0xffff) {
-			sum -= 0xffff;
+		sum += *addr;
+		if (sum > UINT16_MAX) {
+			sum = (sum & UINT16_MAX) + (sum >> 16);
 		}
-		iter++;
+		addr++;
 		remaining_bytes_count -= 2;
 	}
 
 	if (remaining_bytes_count == 1) {
-		sum += *(uint8_t *)iter;
-		if (sum > 0xffff) {
-			sum -= 0xffff;
+		sum += *(uint8_t *)addr;
+		if (sum > UINT16_MAX) {
+			sum = (sum & UINT16_MAX) + (sum >> 16);
 		}
 	}
-	return ~sum;
+	return sum;
+}
+
+uint16_t get_checksum(void *addr, size_t size)
+{
+	assert(addr != NULL);
+
+	return ~get_data_sum((uint16_t *)addr, size);
 }
