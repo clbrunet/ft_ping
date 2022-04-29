deda9b06f55fdd1e17320aaabdd61e36d1018bc3
clbrunet <brunet.clement5@gmail.com>
2022-04-29 09:44:13 +0200

    refactor: extract the recv loop in a function

--- src/ft_ping/main.c
+++ src/ft_ping/main.c
@@ -17,7 +17,7 @@
 
 variables_t g_vars = {0};
 
-void int_handler(int signum)
+static void int_handler(int signum)
 {
 	(void)signum;
 
@@ -26,6 +26,39 @@ void int_handler(int signum)
 	free(g_vars.icmp_request);
 	close(g_vars.socket_fd);
 	exit(0);
+}
+
+static int recv_loop(void)
+{
+	uint8_t msg_iov_base_buf[1000]; // TODO malloc
+	struct iovec msg_iov[1] = {
+		[0].iov_base = msg_iov_base_buf,
+		[0].iov_len = 1000,
+	};
+	struct msghdr msg = {
+		.msg_iov = msg_iov,
+		.msg_iovlen = 1,
+	};
+	while (true) {
+		ssize_t ret = recvmsg(g_vars.socket_fd, &msg, 0);
+		if (ret == -1) {
+			print_error("recvmsg", strerror(errno));
+			return -1;
+		}
+		printf("\nMessage received\n");
+		print_memory(msg_iov_base_buf, ret);
+		struct iphdr *response_iphdr = (void *)msg_iov_base_buf;
+		printf("IP header\n");
+		printf("saddr %u\n", response_iphdr->saddr);
+		printf("daddr %u\n", response_iphdr->daddr);
+		struct icmphdr *response_icmphdr = (void *)msg_iov_base_buf + 20;
+		printf("ICMP header\n");
+		printf("type %hhu\n", response_icmphdr->type);
+		printf("code %hhu\n", response_icmphdr->code);
+		printf("checksum %hu\n", response_icmphdr->checksum);
+		printf("id %hu or %hu\n", response_icmphdr->un.echo.id, ntohs(response_icmphdr->un.echo.id));
+		printf("seq %hu or %hu\n", response_icmphdr->un.echo.sequence, ntohs(response_icmphdr->un.echo.sequence));
+	}
 }
 
 int main(int argc, char *argv[])
@@ -45,43 +78,18 @@ int main(int argc, char *argv[])
 		return 2;
 	}
 
+	signal(SIGINT, &int_handler);
+
 	if (send_icmp_request() == -1) {
 		free(g_vars.icmp_request);
 		close(g_vars.socket_fd);
 		return 2;
 	}
 
-	signal(SIGINT, &int_handler);
-
-	uint8_t msg_iov_base_buf[1000] = {0};
-	struct iovec msg_iov[1] = {
-		[0].iov_base = msg_iov_base_buf,
-		[0].iov_len = 1000,
-	};
-	struct msghdr msg = {
-		.msg_iov = msg_iov,
-		.msg_iovlen = 1,
-	};
-	while (true) {
-		ssize_t ret = recvmsg(g_vars.socket_fd, &msg, 0);
-		if (ret == -1) {
-			print_error("recvmsg", strerror(errno));
-			free(g_vars.icmp_request);
-			close(g_vars.socket_fd);
-			return 2;
-		}
-		printf("\nMessage received\n");
-		print_memory(msg_iov_base_buf, ret);
-		struct iphdr *response_iphdr = (void *)msg_iov_base_buf;
-		printf("IP header\n");
-		printf("saddr %u\n", response_iphdr->saddr);
-		printf("daddr %u\n", response_iphdr->daddr);
-		struct icmphdr *response_icmphdr = (void *)msg_iov_base_buf + 20;
-		printf("ICMP header\n");
-		printf("type %hhu\n", response_icmphdr->type);
-		printf("code %hhu\n", response_icmphdr->code);
-		printf("checksum %hu\n", response_icmphdr->checksum);
-		printf("id %hu or %hu\n", response_icmphdr->un.echo.id, ntohs(response_icmphdr->un.echo.id));
-		printf("seq %hu or %hu\n", response_icmphdr->un.echo.sequence, ntohs(response_icmphdr->un.echo.sequence));
+	if (recv_loop() == -1) {
+		free(g_vars.icmp_request);
+		close(g_vars.socket_fd);
+		return 2;
 	}
+	return 0;
 }
